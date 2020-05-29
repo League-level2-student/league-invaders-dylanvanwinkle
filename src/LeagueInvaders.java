f6707e260211a8943a956c755c15e685a3c1e736
dylanvanwinkle <dylanvanwinkle@gmail.com>
2020-05-29 13:28:24 -0700

    LEaguInvadersIP

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,25 @@
 
-public class LeagueInvaders {
+import javax.swing.JFrame;
 
+public class LeagueInvaders {
+	JFrame frame;
+	public static final int HEIGHT = 800;
+	public static final int WIDTH = 500;
+	GamePanel gp;
+	
+	public static void main(String[] args) {
+		LeagueInvaders li = new LeagueInvaders(new JFrame(), new GamePanel());
+		li.setup();
+	}
+	
+	LeagueInvaders(JFrame frame, GamePanel gp){
+		this.frame = frame;
+		this.gp = gp;
+	}
+	public void setup() {
+		frame.add(gp);
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
